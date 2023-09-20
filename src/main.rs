1cf5b3de6b3b863962ac1e018e23cab3f425efdb
unknown <ChengHKH@gmail.com>
2023-09-20 03:17:56 +0100

    implement lenia calculations

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,77 @@
-use std::f32::consts::TAU;
+use std::{f32::consts::TAU, iter};
 
-use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+use bevy::{prelude::*, sprite::{MaterialMesh2dBundle, Mesh2dHandle}};
 use rand::prelude::*;
 
 #[derive(Component)]
 struct Creature;
 
 #[derive(Component)]
+struct Parameters {
+    mu_k: f32,
+    sigma_k: f32,
+    w_k: f32,
+    
+    mu_g: f32,
+    sigma_g: f32,
+
+    c_rep: f32,
+}
+
+impl Default for Parameters {
+    fn default() -> Self {
+        Self {
+            mu_k: 4.0,
+            sigma_k: 1.0,
+            w_k: 0.022,
+            
+            mu_g: 0.6,
+            sigma_g: 0.15,
+            
+            c_rep: 1.0,
+        }
+    }    
+}
+
+#[derive(Component)]
 struct Particle;
+
+#[derive(Component)]
+struct Fields {
+    R_val: f32,
+    R_grad: Vec3,
+
+    U_val: f32,
+    U_grad: Vec3,
+
+    E_grad: Vec3,
+}
+
+impl Default for Fields {
+    fn default() -> Self {
+        Self {
+            R_val: 0.0,
+            R_grad: Vec3::ZERO,
+
+            U_val: 0.0,
+            U_grad: Vec3::ZERO,
+
+            E_grad: Vec3::ZERO,
+        }
+    }
+}
 
 #[derive(Bundle)]
 struct CreatureBundle {
     spatial: SpatialBundle,
+    parameters: Parameters,
     creature: Creature,
 }
 
 #[derive(Bundle)]
 struct ParticleBundle {
     materialmesh2d: MaterialMesh2dBundle<ColorMaterial>,
+    fields: Fields,
     particle: Particle,
 }
 
@@ -25,14 +79,17 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, (setup, spawn_creature))
-        .add_systems(Update, bevy::window::close_on_esc)
+        .add_systems(Update, (bevy::window::close_on_esc, calculate_fields))
+        .add_systems(Update, (update_position, update_size).after(calculate_fields))
         .run();
 }
 
 fn setup(
     mut commands: Commands,
 ) {
-    commands.spawn(Camera2dBundle::default());
+    let mut camera = Camera2dBundle::default();
+    camera.projection.scale = 1.0 / 24.0;
+    commands.spawn(camera);
 }
 
 fn spawn_creature(
@@ -47,21 +104,108 @@ fn spawn_creature(
             transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)),
             ..default()
         },
+        parameters: Parameters::default(),
         creature: Creature,
     }).with_children(|parent| {
-        for _ in 0..200 {
-            let r = 150.0 * rng.gen::<f32>().sqrt();
+        for _ in 0..199 {
+            let r = 10.0 * rng.gen::<f32>().sqrt();
             let theta = rng.gen::<f32>() * TAU;
 
             parent.spawn(ParticleBundle {
                 materialmesh2d: MaterialMesh2dBundle {
-                    mesh: meshes.add(shape::Circle::new(5.).into()).into(),
+                    mesh: meshes.add(shape::Circle::new(0.5).into()).into(),
                     material: materials.add(ColorMaterial::from(Color::WHITE)),
                     transform: Transform::from_translation(Vec3::new(r * theta.cos(), r * theta.sin(), 0.0)),
                     ..default()
                 },
+                fields: Fields::default(),
                 particle: Particle,
             });
         }
     });
+}
+
+fn repulsion_field(r: f32, c_rep: f32) -> (f32, f32) {
+    let t = f32::max(0.0, 1.0 - r);
+    (0.5 * c_rep * t * t, -c_rep * t)
+}
+
+fn radial_field(x: f32, mu: f32, sigma: f32, w: f32) -> (f32, f32) {
+    let t = (x - mu) / sigma;
+    let y = w / (t * t).exp();
+    (y, -2.0 * t * y / sigma)
+}
+
+fn calculate_fields(
+    creature_query: Query<(&Parameters, &Children), With<Creature>>,
+    mut particle_query: Query<(&Transform, &mut Fields), With<Particle>>,
+) {
+    for (parameters, children) in creature_query.iter() {
+        for child in children.iter() {
+            let (_, mut fields) = particle_query.get_mut(*child).unwrap();
+            fields.R_val = repulsion_field(0.0, parameters.c_rep).0;
+            fields.R_grad = Vec3::ZERO;
+            
+            fields.U_val = radial_field(0.0, parameters.mu_k, parameters.sigma_k, parameters.w_k).0;
+            fields.U_grad = Vec3::ZERO;
+
+            fields.E_grad = Vec3::ZERO;
+        }
+
+        for (child_i, child_j) in children.iter()
+            .enumerate()
+            .flat_map(|(index, child)| iter::zip(
+                iter::repeat(child),
+                children.iter().skip(index + 1),
+            ))
+        {
+            let [(transform_i, mut fields_i), (transform_j, mut fields_j)] = particle_query.get_many_mut([*child_i, *child_j]).unwrap();
+            
+            let r = transform_i.translation.distance(transform_j.translation);
+            let r_grad = (transform_i.translation - transform_j.translation) / r;
+
+            if r < 1.0 {
+                let (R, dR) = repulsion_field(r, parameters.c_rep);
+                fields_i.R_val += R;
+                fields_j.R_val += R;
+                fields_i.R_grad += r_grad * dR;
+                fields_j.R_grad -= r_grad * dR;
+            }
+
+            let (K, dK) = radial_field(r, parameters.mu_k, parameters.sigma_k, parameters.w_k);
+            fields_i.U_val += K;
+            fields_j.U_val += K;
+            fields_i.U_grad += r_grad * dK;
+            fields_j.U_grad -= r_grad * dK;
+        }
+
+        for child in children.iter() {
+            let (_, mut fields) = particle_query.get_mut(*child).unwrap();
+            let (_, dG) = radial_field(fields.U_val, parameters.mu_g, parameters.sigma_g, 1.0);
+            fields.E_grad = fields.R_grad - (dG * fields.U_grad);
+        }
+    }
+}
+
+fn update_position(
+    time: Res<Time>,
+    mut particle_query: Query<(&mut Transform, &Fields), With<Particle>>,
+) {
+    for (mut transform, fields) in particle_query.iter_mut() {
+        transform.translation += 0.1 * (-fields.E_grad);
+    }
+}
+
+fn update_size(
+    mut meshes: ResMut<Assets<Mesh>>,
+    creature_query: Query<(&Parameters, &Children), With<Creature>>,
+    mut particle_query: Query<(&Mesh2dHandle, &Fields), With<Particle>>,
+) {
+    for (parameters, children) in creature_query.iter() {
+        for child in children.iter() {
+            let (mesh2dhandle, fields) = particle_query.get(*child).unwrap();
+            let r = parameters.c_rep / (fields.R_val * 5.0);
+            let _ = meshes.set(&mesh2dhandle.0, shape::Circle::new(r).into());
+        }
+    }
 }
